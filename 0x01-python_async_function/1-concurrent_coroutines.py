199335e5b8837ff0bb673a426f93b00d4afbb8fe
nickfff-dev <hunchographics@gmail.com>
2024-03-21 23:48:23 +0300

    pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     An asynchronous coroutine that spawns wait_random n
     times with the specified max_delay.

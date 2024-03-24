001462c31cec12c11cd984dafb7b6f5efbcabfd0
nickfff-dev <hunchographics@gmail.com>
2024-03-24 23:54:41 +0300

    refactor

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,32 +3,20 @@
 Safely retrieves a value from a dictionary by key
 """
 
-from typing import List, Tuple, TypeVar, Union
-
-T = TypeVar('T')
+from typing import List, Tuple, Any
 
 
-def zoom_array(lst: List[T], factor: int = 2) -> List[T]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms an array by repeating each element a specified number of times.
-
-    Parameters:
-    lst (List[T]): The list to zoom.
-    factor (int, optional): The number of times to repeat each element.
-    Defaults to 2.
-
-    Returns:
-    List[T]: The zoomed list.
     """
-    zoomed_in: List[T] = [
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
 array = [12, 72, 91]
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
+zoom_2x = zoom_array(tuple(array))
+zoom_3x = zoom_array(tuple(array), int(5.0))

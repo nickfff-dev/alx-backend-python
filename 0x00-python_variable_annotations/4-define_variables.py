7e0448b89044efe89f5e05cedc70ff4bf8450ccb
nickfff-dev <hunchographics@gmail.com>
2024-03-24 23:06:08 +0300

    annotating variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+This module defines and annotates variables with specified values.
+
+The variables defined in this module are:
+- a: an integer with a value of 1
+- pi: a float with a value of 3.14
+- i_understand_annotations: a boolean with a value of True
+- school: a string with a value of "Holberton"
+This module is designed to be imported and used in other Python scripts.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"

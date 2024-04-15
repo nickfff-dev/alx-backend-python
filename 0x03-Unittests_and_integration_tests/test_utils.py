9863b0cd42ad2b276721e56a202ee09419d86ab4
nickfff-dev <hunchographics@gmail.com>
2024-04-15 21:23:05 +0300

    mock requests.get

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Generic utilities for github org client."""
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,6 +28,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertIn(path[-1], str(context.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """Test class for the get_json function in utils."""
+    @parameterized.expand([
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that get_json returns the expected results."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
+        mock_get.reset_mock()
 
 
 if __name__ == '__main__':
